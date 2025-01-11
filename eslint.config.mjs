cbcd759ef19263dc2514c4c1a0806711082c4697
yuichisugio <yuichisugio@users.noreply.github.com>
2025-01-11 17:37:30 +0900

    ESLint,Prettier,husky&lint-stagedの設定 #3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+import unicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +15,69 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": typescript,
+      unicorn: unicorn,
+      import: importPlugin,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      ...(typescript.configs?.["recommended-type-checked"]?.rules ?? {}),
+      ...(typescript.configs?.["stylistic-type-checked"]?.rules ?? {}),
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: { attributes: false },
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+        },
+      ],
+      "func-style": ["error", "declaration", { allowArrowFunctions: false }],
+      "prefer-arrow-callback": ["error", { allowNamedFunctions: false }],
+      "import/no-default-export": "error",
+    },
+  },
+  {
+    files: [
+      "**/page.tsx",
+      "**/layout.tsx",
+      "next.config.ts",
+      "postcss.config.mjs",
+      "tailwind.config.ts",
+    ],
+    rules: {
+      "import/no-default-export": "off",
+      "import/prefer-default-export": "error",
+    },
+  },
+  {
+    ignores: ["src/components/ui/*", "*.md"],
+  },
 ];
 
 export default eslintConfig;
