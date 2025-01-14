9604629627341d76eadf5e70a7f13f4555d78665
yuichisugio <yuichi.sugio@cchan.tv>
2025-01-14 13:17:52 +0900

    Shadcn/uiのインストール

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,19 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  plugins: [],
+    darkMode: ["class"],
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  plugins: [tailwindcssAnimate],
+    theme: {
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {}
+    	}
+    }
 };
 export default config;
