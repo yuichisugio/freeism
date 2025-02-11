72c8f82517d313c042feb63473cea67fcd2205a8
yuichisugio <yuichisugio@users.noreply.github.com>
2025-02-11 12:34:16 +0900

    fix(other): eslintエラー

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <header></header>
+    <div className="flex min-h-screen flex-col">
+      <header className="w-full"></header>
 
-      <main>
+      <main className="flex-1">
         <iframe
           src="https://docs.google.com/document/d/e/2PACX-1vSv2DzoMvPnYK4EQQn2q8jwSch9-YV3LrNUC42CcFxJoM4lWWfw_C6BbCtLxwHVTiw-FITAF1U1rl0u/pub?embedded=true"
           width="100%"
           height="1000"
-        ></iframe>
+          title="Embedded Google Document"
+        />
       </main>
 
-      <footer>
+      <footer className="flex w-full justify-center gap-8 py-6">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
