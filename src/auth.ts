5a187c64743956f3c472805e654c28ccdf0fac04
yuichisugio <yuichi.sugio@cchan.tv>
2025-02-12 17:42:06 +0900

    auth.jsの環境変数とPrisma ORMの設定

--- src/auth.ts
+++ src/auth.ts
@@ -3,25 +3,11 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-  session: {
-    strategy: "database",
-  },
-  pages: {
-    signIn: "/auth/signin",
-  },
+  providers: [Google],
+  session: { strategy: "database" },
+  pages: { signIn: "/auth/signin" },
   callbacks: {
     async session({ session, user }) {
       if (session.user) {
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
