5c8301160112bcd07ab42ff799a988bb9b8c58bd
yuichisugio <yuichi.sugio@cchan.tv>
2025-02-20 14:01:24 +0900

    微修正

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
+// ログインボタンの引数は入れない。カスタムログインページに遷移させたいため。
 export function LoginButton() {
   return (
     <Button
