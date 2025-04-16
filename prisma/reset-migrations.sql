eb3bbef0718cbd756bb8d91939e1d68562338997
yuichisugio <yuichisugio@users.noreply.github.com>
2025-04-16 23:13:10 +0900

    fix: s

--- prisma/reset-migrations.sql
+++
@@ -1,8 +0,0 @@
--- 失敗したマイグレーションステータスをリセットするためのスクリプト
-UPDATE "_prisma_migrations"
-SET "applied_steps_count" = 0,
-    "rolled_back" = TRUE,
-    "rolled_back_at" = NOW()
-WHERE "migration_name" = '20250307092624_remove_group_status'
-AND "finished_at" IS NOT NULL
-AND "rolled_back" = FALSE; 
